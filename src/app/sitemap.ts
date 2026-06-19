1d0322197fdec6272971ef469d1a96e0c9c2766d
Manuel <yesas12345678@gmail.com>
2026-06-19 16:41:40 +0200

    make sitemap dynamic so it is server-rendered on demand

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next";
 import { pool } from "@/lib/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://mihogarasegurado.com";
 
